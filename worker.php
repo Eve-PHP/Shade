0434cae1308f93c687d5d04ca01c405bac143a3d
soycaringal <soycaringal@github.com>
2015-10-20 20:13:21 +0800

    fix worker.php template

--- worker.php
+++ worker.php
@@ -1,6 +1,21 @@
 <?php 
-	require_once __DIR__ . '/vendor/autoload.php';
-	
-	use Eve\Framework\Qdispatch;
+require_once __DIR__ . '/vendor/autoload.php';
 
-	Qdispatch::i('localhost', 5672, 'guest', 'guest')->run();
+// we need to manually set this
+$_SERVER['HTTP_HOST']   = null;
+// we need to manually set this
+$_SERVER['REQUEST_URI'] = null;
+
+// we're going to setup virtual server so
+// jobs and models will not loose it's original scope
+Eve\Framework\Index::i(dirname(__DIR__), 'Eve')
+// set default paths
+->defaultPaths()
+// set default database
+->defaultDatabases();
+
+use Eve\Framework\Qdispatch;
+
+Qdispatch::i('localhost', 5672, 'guest', 'guest')->run();
+
+?>
